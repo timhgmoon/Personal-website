157f825ff401cb1fff434d631309732978fe4e52
Timothy Moon <tim@Timothys-MacBook-Pro.local>
2021-10-14 12:00:51 -0700

    added yellow text on hover to top navigation. Fixed buggy on hover for images. Added animation for tablet view

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-$("#rocket-image").on("load", function(){
+$("document").ready(function(){
   $("#rocket-image").animate({right: 100, top: 140}, 5000);
+})
+
+$project1 = $('#project-1');
+$project2 = $('#project-2');
+$todoBanner = $('#todo-banner');
+$responsiveBanner = $('#responsive-banner');
+
+//todo list mouse in/mouse out functions
+//fade img and brings up project banner
+$project1.on("mouseenter", function() {
+  $project1.css("opacity", '.5');
+  $todoBanner.addClass('toggle');
+})
+
+$project1.on("mouseleave", function() {
+  $project1.css("opacity", '1');
+  $todoBanner.removeClass('toggle');
+})
+
+//responsive design mouse in/mouse out functions
+$project2.on("mouseover", function() {
+  $project2.css("opacity", '.5')
+  $responsiveBanner.addClass('toggle');
+})
+
+$project2.on("mouseleave", function() {
+  $project2.css("opacity", '1');
+  $responsiveBanner.removeClass('toggle');
 })
