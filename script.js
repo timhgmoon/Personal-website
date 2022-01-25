18288d86c665d1daddf6ff86c15e2a747a69eac8
Timothy Moon <timhgmoon@gmail.com>
2022-01-24 19:15:31 -0800

    added 3rd project

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 $project1 = $('#project-1');
 $project2 = $('#project-2');
+$project3 = $('#project-3');
 $todoBanner = $('#driver-banner');
 $responsiveBanner = $('#responsive-banner');
+$socialMediaBanner = $('#social-media-banner');
 
 //todo list mouse in/mouse out functions
 //fade img and brings up project banner
@@ -25,4 +27,15 @@ $project2.on("mouseover", function() {
 $project2.on("mouseleave", function() {
   $project2.css("opacity", '1');
   $responsiveBanner.removeClass('toggle');
+})
+
+//social media mouse in/mouse out functions
+$project3.on("mouseover", function() {
+  $project3.css("opacity", '.5')
+  $socialMediaBanner.addClass('toggle');
+})
+
+$project3.on("mouseleave", function() {
+  $project3.css("opacity", '1');
+  $socialMediaBanner.removeClass('toggle');
 })
